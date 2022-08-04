5c83c2121df30e999cfe12c71752e072cfd0ade0
maximys5582 <109856835+maximys5582@users.noreply.github.com>
2022-08-04 13:46:35 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,51 @@
+const todosNode = document.querySelector('.todoList');
+const inputNode = document.querySelector('.js-input');
+const btnNode = document.querySelector('js-btn');
+
+let todos = [];
+
+function addTodo(text){
+    const todo = {
+        text,
+        done: false,
+        id: `${Math.random()}`
+    };
+
+    todos.push(todo);
+}
+
+function deleteTodo(id) {
+    todos.forEach(todo => {
+        if (todo.id === id) {
+            todo.done = true;
+        }
+    })
+}
+
+
+function render(){
+    console.log(todos);
+    let html = '';
+
+    todos.forEach(todo => {
+        if(todo.done){
+            return;
+        };
+
+        html += `
+        <div>${todo.text}</div>
+        `;
+    })
+
+    todosNode.innerHTML = html;
+}
+
+btnNode.addEventListener('click', () => {
+    const text = inputNode.value;
+    
+    addTodo(text);
+
+    render();
+});
+
+render();
